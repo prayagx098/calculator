5107fff63a8a20a95846a990ebc8c2e1842057de
prayag <prayagjishnu123@gmail.com>
2024-05-17 11:42:13 +0530

    calculator

---
+++ static/javascript/script.js
@@ -0,0 +1,67 @@
+let currentInput = '';
+let operator = '';
+let result = 0;
+let DisplayInput='';
+
+function appendNumber(number) {
+  currentInput += number;
+  DisplayInput =currentInput;
+  updateDisplay();
+}
+
+function setOperator(op) {
+  operator = op;
+  result = parseFloat(currentInput);
+  currentInput += operator;
+  DisplayInput=currentInput;
+  currentInput = '';
+  
+  updateDisplay();
+}
+
+function clearLast(){
+  backnum = currentInput;
+  backnum=Math.floor(backnum / 10);
+  currentInput=backnum;
+  DisplayInput=currentInput;
+  updateDisplay();
+
+}
+
+function calculate() {
+  const inputNumber = parseFloat(currentInput);
+  switch (operator) {
+    case '+':
+      result += inputNumber;
+      break;
+    case '-':
+      result -= inputNumber;
+      break;
+    case '*':
+      result *= inputNumber;
+      break;
+    case '/':
+      if (inputNumber !== 0) {
+        result /= inputNumber;
+      } else {
+        result = 'Error';
+      }
+      break;
+    default:
+      result = inputNumber;
+  }
+  DisplayInput = result.toString();
+  updateDisplay();
+}
+
+function clearDisplay() {
+  currentInput = '';
+  operator = '';
+  result = 0;
+  DisplayInput = '';
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById('display').innerText = DisplayInput || '0';
+}
